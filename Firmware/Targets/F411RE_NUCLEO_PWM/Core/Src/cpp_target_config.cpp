5d223c8f3a80cce69e9499e3360f612aaf8a5431
Ian Murphy <ian.murphy@yahoo.co.uk>
2023-01-24 21:55:10 +0000

    F411RE_NUCLEO_PWM target compiles.

---
+++ Firmware/Targets/F411RE_NUCLEO_PWM/Core/Src/cpp_target_config.cpp
@@ -0,0 +1,27 @@
+#include "cpp_target_config.h"
+
+#ifdef HSPIDRV
+extern SPI_HandleTypeDef hspi1;
+static const std::vector<OutputPin> external_spi_cspins{OutputPin(*SPI2_NSS_GPIO_Port, SPI2_NSS_Pin)};
+SPIPort external_spi{hspi2,external_spi_cspins,true};
+#endif
+
+#ifdef HSPI2
+extern SPI_HandleTypeDef hspi2;
+static const std::vector<OutputPin> motor_spi_cspins{OutputPin(*SPI1_SS1_GPIO_Port, SPI1_SS1_Pin), OutputPin(*SPI1_SS2_GPIO_Port, SPI1_SS2_Pin),OutputPin(*SPI1_SS3_GPIO_Port, SPI1_SS3_Pin)};
+SPIPort motor_spi{hspi1,motor_spi_cspins,false};
+#endif
+
+#ifdef UART_PORT_EXT
+extern UART_HandleTypeDef UART_PORT_EXT;
+UARTPort external_uart{UART_PORT_EXT};
+#endif
+
+#ifdef I2C_PORT
+extern I2C_HandleTypeDef I2C_PORT;
+I2CPort i2cport{I2C_PORT};
+#endif
+
+#ifdef DEBUGPIN
+const OutputPin debugpin = OutputPin(*GP1_GPIO_Port, GP1_Pin);
+#endif
